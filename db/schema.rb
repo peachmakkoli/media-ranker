69c1f7e58683f0600cf3e9e9fec894a6e98a40af
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-15 00:53:12 -0700

    fixes db:migrate error by changing order of migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_15_074755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "work_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string "category"
+    t.string "title"
+    t.string "creator"
+    t.string "publication_year"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
