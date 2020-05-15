ad68a883e322b9c1151b03c6e94dd0ff14f2d3c8
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-15 00:37:42 -0700

    generates votes controller, sets nested route within works

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'votes/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homepages#index'
   
-  resources :works
+  resources :works do 
+    post "/upvote", to: "votes#create" 
+  end
 
   resources :users, only: [:index, :show]
   get "/login", to: "users#login_form", as: "login"
