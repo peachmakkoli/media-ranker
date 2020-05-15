a5b20b7cc6cd778e286fadbd37b5d210acab5e5e
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-15 00:51:04 -0700

    relates work and use to votes, adds model validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,5 @@
 class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :work
+  validates :user_id, :work_id, presence: true
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,5 @@
 class Work < ApplicationRecord
+  has_many :votes
+  validates :category, :title, presence: true
+  validates :title, uniqueness: { scope: category, case_sensitive: false }
 end
