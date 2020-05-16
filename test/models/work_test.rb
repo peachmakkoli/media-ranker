338b982498521f873c51c205b8fc3d896618898d
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-16 14:45:11 -0700

    adds a test for relationships, work can have many votes, creates users test fixtures

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,6 +3,9 @@ require "test_helper"
 describe Work do
   before do
     @album = works(:album)
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+    @user3 = users(:user3)
   end
   
   it "can be instantiated" do
@@ -16,7 +19,16 @@ describe Work do
   end
 
   describe "relationships" do
-
+    it "can have many votes" do
+      vote_1 = Vote.create!(work_id: @album.id, user_id: @user1.id)
+      vote_2 = Vote.create!(work_id: @album.id, user_id: @user2.id)
+      vote_3 = Vote.create!(work_id: @album.id, user_id: @user3.id)
+      
+      expect(@album.votes.count).must_equal 3
+      @album.votes.each do |vote|
+        expect(vote).must_be_instance_of Vote
+      end
+    end
   end
 
   describe "validations" do
