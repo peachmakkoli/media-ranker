7a17455c895e44ce34796c570c69af26f25b60f1
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-16 14:35:46 -0700

    adds tests for work model instantiation

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @album = works(:album)
+  end
+  
+  it "can be instantiated" do
+    expect(@album.valid?).must_equal true
+  end
+
+  it "will have the required fields" do
+    [:category, :title, :creator, :publication_year, :description].each do |field|
+      expect(@album).must_respond_to field
+    end
+  end
+
+  describe "relationships" do
+
+  end
+
+  describe "validations" do
+  
+  end
+
+  describe "sort works" do
+    
+  end
+
+  describe "top ten" do
+    
+  end
 end
