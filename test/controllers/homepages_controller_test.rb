95c840648808ab498c5d5af2efd320b38102bcca
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-16 12:18:47 -0700

    adds homepages#index action, logic to find the media spotlight, helper method to find the top ten of each category

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 describe HomepagesController do
-  it "must get index" do
-    get homepages_index_url
+  it "can get the homepage" do
+    get root_path
+
     must_respond_with :success
   end
-
 end
