0efb77d83557c03015fd1c0e2f1a294a64a8e2f3
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-16 14:11:40 -0700

    adds sort_works and top_ten custom methods, which need to be moved from the controllers

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,12 @@ class Work < ApplicationRecord
   has_many :votes
   validates :category, :title, presence: true
   validates :title, uniqueness: { scope: :category, case_sensitive: false }
+
+  def sort_works
+    # move this from works_controller into model
+  end
+
+  def top_ten
+    # move this from homepages_controller into model
+  end
 end
