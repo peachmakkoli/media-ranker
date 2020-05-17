bebc940e895709cb70b95c53d6508f135b5d8869
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-17 16:32:03 -0700

    adds validation to vote model, ensures users can vote for a work only once

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
   validates :user_id, :work_id, presence: true
+  validates :user, uniqueness: { scope: :work_id, message: "has already voted for this work" }
 end
