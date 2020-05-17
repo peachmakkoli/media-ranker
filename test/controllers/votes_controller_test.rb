eba4adb5b8e6e5eacd6a3f599b109800eab6af49
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-17 15:28:07 -0700

    adds placeholder blocks for create, validate_work, validate_user tests

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 describe VotesController do
-  it "must get create" do
-    get votes_create_url
-    must_respond_with :success
+  describe "create" do
+    
   end
 
+  describe "validate work" do
+
+  end
+
+  describe "validate user" do
+
+  end
 end
