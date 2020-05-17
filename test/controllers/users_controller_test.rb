6777fe4f63b86295405502760f2b50a03e4526c3
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-17 01:54:55 -0700

    replaces auto-generated tests with describe blocks for each users action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,29 +1,27 @@
 require "test_helper"
 
 describe UsersController do
-  it "must get index" do
-    get users_index_url
-    must_respond_with :success
+  describe "index" do
+
   end
 
-  it "must get show" do
-    get users_show_url
-    must_respond_with :success
+  describe "show" do
+
   end
 
-  it "must get login_form" do
-    get users_login_form_url
-    must_respond_with :success
+  describe "login form" do
+
+  end
+  
+  describe "login" do
+  
   end
 
-  it "must get login" do
-    get users_login_url
-    must_respond_with :success
+  describe "current" do
+
   end
 
-  it "must get logout" do
-    get users_logout_url
-    must_respond_with :success
-  end
+  describe "logout" do
 
+  end
 end
