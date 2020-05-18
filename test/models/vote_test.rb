877b0d913835b6ccbdcd1352925cf749f7f3db20
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-17 20:15:42 -0700

    adds pseudocode for user and work methods in the vote model, tests for instantiation

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @album = works(:album)
+    @album2 = works(:album2)
+    @album3 = works(:album3)
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+    @user3 = users(:user3)
+
+    @vote = Vote.create(
+      work_id: @album.id,
+      user_id: @user1.id
+    )
+  end
+  
+  it "can be instantiated" do
+    expect(@vote.valid?).must_equal true
+  end
+
+  it "will have the required fields" do
+    [:work_id, :user_id].each do |field|
+      expect(@vote).must_respond_to field
+    end
+  end
 end
