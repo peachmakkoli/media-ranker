348ca75f0bd213818719d2ba2239df7c02316ffc
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-17 18:39:58 -0700

    updates flash[:success] message for new user creation so it matches the example project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:success] = "Successfully logged in as new user #{@user.username}"
+      flash[:success] = "Successfully created new user #{@user.username} with ID #{@user.id}"
       redirect_to root_path
       return
     else
