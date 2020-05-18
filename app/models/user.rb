c1a7b8ae7f68d4c720bb18c24389184ced904fe3
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-17 18:20:30 -0700

    adds pseudocode for upvoted_works method in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :votes
   validates :username, presence: true, uniqueness: true
+
+  def upvoted_works(user)
+    # throw exception if the user is invalid
+    # loop through user.votes
+    # lookup Works table, return all Works whose id matches vote.work_id
+  end
 end
