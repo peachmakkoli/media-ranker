f0ce755a579667557149f6760c2df09d57527f81
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-18 19:39:44 -0700

    refactors work #sort_works, #top_ten, and #spotlight methods to work with counter_cache, all tests passing

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,27 +4,24 @@ class Work < ApplicationRecord
   validates :category, inclusion: { in: ["album", "book", "movie"] }
   validates :title, uniqueness: { scope: :category, case_sensitive: false }
 
-  # I chose to design a sort_works (by vote) method instead of creating a "votes" column in the works table, because there was a possibility that values in the votes column would not match up the actual number of vote records in the database
+  # all custom methods in the work model take advantage of counter_cache, inspired by cristian's solution: https://stackoverflow.com/a/24151460
   def self.sort_works(category)
     raise ArgumentError.new("Error: Wrong argument passed in to Work.sort_works method") if !["album", "book", "movie"].include?(category)
 
-    return Work.where(category: category).joins(:votes).group("works.id").order("count(work_id) DESC")
+    return Work.where(category: category).order('votes_count DESC')
   end 
 
   def self.top_ten(category)
     raise ArgumentError.new("Error: Wrong argument passed in to Work.top_ten method") if !["album", "book", "movie"].include?(category)
 
-    return Work.where(category: category).limit(10).sort_by{ |work| work.votes.count }.reverse!
+    return Work.where(category: category).order('votes_count DESC').limit(10)
   end
 
   def self.spotlight
-    highest = Work.all.map{ |work| work.votes.count }.max
-    spotlight = Work.joins(:votes).group("works.id").having("count(work_id) = ?", highest).order("max(votes.created_at) DESC") # retrieves a list of works that have the highest number of votes, ordered by most recent vote (modified from Dorian and Arta's answer: https://stackoverflow.com/a/27772341)
-    raise
     if Work.none? || Vote.none?
       return nil
     else
-      return spotlight.first # if there is a tie, the work that was voted on most recently will be selected
+      return Work.order('votes_count DESC').first # if there is a tie, the work that was voted on most recently will be selected
     end
   end
 end
