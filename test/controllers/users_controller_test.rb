2c81a12debb5073815101c26226aacd2b5486b1f
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-19 19:08:17 -0700

    adds tests for users#index action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,26 @@
 require "test_helper"
 
 describe UsersController do
-  describe "index" do
+  before do
+    @user = users(:user1)
+  end
 
+  describe "index" do
+    it "responds with success when there are many users saved" do
+      expect(User.count).must_equal 3
+
+      get users_path
+      must_respond_with :success
+    end
+
+    it "responds with success when there are no users saved" do
+      expect { 
+        User.destroy_all
+      }.must_differ "User.count", -3
+
+      get users_path
+      must_respond_with :success
+    end
   end
 
   describe "show" do
