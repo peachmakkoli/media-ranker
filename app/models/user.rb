38300ca901b7f8aa0c42981abf4e0b0a17a5cdbc
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-20 03:58:59 -0700

    removes optional self. from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   def upvoted_works
-    return Work.joins(:votes).where(votes: { user_id: self.id })
+    return Work.joins(:votes).where(votes: { user_id: id })
   end
 
   def voted_on(work)
     raise ArgumentError.new("Error: Wrong work_id passed in to user#voted_on method") if !work
     
-    return self.votes.find_by(work_id: work.id).created_at
+    return votes.find_by(work_id: work.id).created_at
   end
 end
