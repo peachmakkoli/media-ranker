b85529e6f773b55e200654468a27691eda22cf68
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-21 22:52:23 -0700

    adds @current_user helper and require_login methods to application, adds find_work and require_login controller filters to votes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
     head :not_found if !@work
     return
   end
+  
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    if !current_user
+      flash[:error] = "A problem occurred: You must log in to do that"
+      redirect_to request.referrer
+      return
+    end
+  end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,10 @@
 class VotesController < ApplicationController
-  def create
-    return if !validate_work || !validate_user
+  before_action do
+    find_work(params[:work_id])
+  end
+  before_action :require_login
 
+  def create
     @vote = Vote.new(
       work_id: @work.id, 
       user_id: @current_user.id
@@ -20,31 +23,6 @@ class VotesController < ApplicationController
   end
 
   private 
-
-  # helper method for checking if the work exists
-  def validate_work
-    @work = Work.find_by(id: params[:work_id])
-
-    if !@work
-      head :not_found
-      return
-    else
-      return true
-    end
-  end
-
-  # helper method for checking if the user is logged in
-  def validate_user
-    @current_user = User.find_by(id: session[:user_id])
-
-    if !@current_user
-      flash[:error] = "A problem occurred: You must log in to do that"
-      redirect_to request.referrer
-      return
-    else
-      return true
-    end
-  end
 
   def vote_params
     return params.require(:vote).permit(:work_id, :user_id)
